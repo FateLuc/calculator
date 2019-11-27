a3e5a24f23e2339d0f29d2d1cff3f33a53e650ea
Feger <luc.feger@bluewin.ch>
2019-11-27 14:15:20 +0100

    last ckommit

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <html>
 <head>
 
@@ -6,26 +5,32 @@
 <body>
 
 <?php
+
+$total = 0;
 if (isset($_POST['number']) && $_POST['number'] != "") {
 
     $data = $_POST['number'];
     $fp = fopen('data.txt', 'a');
-    fwrite($fp, $data);
+    fwrite($fp, $data . "\n");
     fclose($fp);
 
 
     $myfile = fopen("data.txt", "r") or die("Unable to open file!");
-            echo fread($myfile, filesize("data.txt" ));
-        }
-    ?>
-    <form method="post">
-        <input type="text" name="number"> <br> <br>
-        <input type="submit">
-    </form>
-        <form action="clear.php" method="post">
+    echo fread($myfile, filesize("data.txt"));
+}
 
-            <input type="hidden" name="action" value="empty">
-            <input type="submit" value="empty" onclick="" />
-    </form>
+
+?>
+
+
+<form method="post">
+    <input type="text" name="number"> <br> <br>
+    <input type="submit">
+</form>
+<form action="clear.php" method="post">
+
+    <input type="hidden" name="action" value="empty">
+    <input type="submit" value="empty" onclick=""/>
+</form>
 </body>
 </html>
